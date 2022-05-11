5aea436cc954066a24ba5b96139b48bf49fe8e4c
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-05-10 20:45:02 -0700

    created route for get single trainer

--- controllers/api/trainer-routes.js
+++ controllers/api/trainer-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { Trainer } = require("../../models");
 
+// GET all trainers
 router.get('/', (req,res) => {
   console.log('-----------------------');
   Trainer.findAll({
@@ -9,6 +10,41 @@ router.get('/', (req,res) => {
     .then(dbTrainerData => res.json(dbTrainerData))
     .catch(err => {
       console.log(err)
+      res.status(500).json(err);
+    });
+});
+
+// GET single trainer
+router.get('/:id', (req, res) => {
+  Trainer.findOne({
+    attributes: { exclude: ['password'] },
+    where: { id: req.params.id }
+  })
+    .then(dbTrainerData => {
+      if (!dbTrainerData) {
+        res.status(404).json({ message: 'No trainer found with this id' });
+        return;
+      }
+      res.json(dbTrainerData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// Create Trainer
+router.post('/', (req, res) => {
+  Trainer.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(dbTrainerData => {
+      res.json(dbTrainerData);
+    })
+    .catch(err => {
+      console.log(err);
       res.status(500).json(err);
     });
 });
