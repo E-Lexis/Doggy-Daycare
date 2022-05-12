37664927e92c6cd21fe2a313d1a2a8f1db6dd72b
Mo Yang <moyang313@gmail.com>
2022-05-11 21:34:30 -0400

    add owner seed and connection

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,5 @@
 const seedTrainers = require('./trainer-seed');
+const seedOwners = require('./owner-seed');
 
 const sequelize = require('../config/connection')
 
@@ -6,6 +7,8 @@ const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('----------------------');
   await seedTrainers();
+  console.log('----------------------');
+  await seedOwners();
   console.log('----------------------');
 
   process.exit(0);
---
+++ seeds/owner-seed.js
@@ -0,0 +1,29 @@
+const { Owner } = require('../models')
+
+const Ownerdata = [
+  {
+    username: "OwnerOne",
+    password: "password1234",
+    email: "owner@mail.com",
+    phone: "313-111-1111",
+    dog_id: "1"
+  },
+  {
+    username: "OwnerTwo",
+    password: "password1234",
+    email: "scheduler@mail.com",
+    phone: "313-222-2222",
+    dog_id: "2"
+  },
+  {
+    username: "OwnerThree",
+    password: "password1234",
+    email: "LeadOwner@mail.com",
+    phone: "313-333-3333",
+    dog_id: "3"
+  }
+];
+
+const seedOwners = () => Owner.bulkCreate(Ownerdata)
+
+module.exports = seedOwners
