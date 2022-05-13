c1bc04e5b11bc78e90975b54ba5b337406f76dfb
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-05-13 01:22:00 -0700

    all code is in, need to figure out more about multiple secrets and .env files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,39 +1,71 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const sequelize = require("../config/connection");
-const { Owner, Trainer, Dog } = require('../models');
+const { Owner, Trainer, Dog } = require("../models");
+
 
 router.get("/", (req, res) => {
   console.log("======================");
   Dog.findAll({
-    attributes: [
-      'name',
-      'age',
-      'breed',
-      'size'
-    ],
+    attributes: ["name", "age", "breed", "size"],
     include: [
       {
         model: Owner,
-        attributes: ['username']
-      }
-    ] 
+        attributes: ["username"],
+      },
+    ],
   })
-    .then(dbDogData => {
-      const dogs = dbDogData.map(post => post.get({ plain:true }))
-      res.render('homepage', { dogs });
+    .then((dbDogData) => {
+      const dogs = dbDogData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { dogs });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-router.get("/login", (req, res) => {
-  res.render("login");
-});
+// const passport = require("passport");
+// var OpenIDConnectStrategy = require("passport-openidconnect");
 
-router.get("/signup", (req, res) => {
-  res.render("signup");
-});
+// passport.use(
+//   new OpenIDConnectStrategy(
+//     {
+//       issuer: "https://" + process.env.test["AUTH0_DOMAIN"] + "/",
+//       authorizationURL: "https://" + process.env.test["AUTH0_DOMAIN"] + "/authorize",
+//       tokenURL: "https://" + process.env.test["AUTH0_DOMAIN"] + "/oauth/token",
+//       userInfoURL: "https://" + process.env.test["AUTH0_DOMAIN"] + "/userinfo",
+//       clientID: process.env.test["AUTH0_CLIENT_ID"],
+//       clientSecret: process.env.test["AUTH0_CLIENT_SECRET"],
+//       callbackURL: "/oauth2/redirect",
+//       scope: ["profile"],
+//     },
+//     function verify(issuer, profile, cb) {
+//       return cb(null, profile);
+//     }
+//   )
+// );
+
+// passport.serializeUser(function(user, cb) {
+//   process.nextTick(function() {
+//     cb(null, { id: user.id, username: user.username, name: user.displayName });
+//   });
+// });
+
+// passport.deserializeUser(function(user, cb) {
+//   process.nextTick(function() {
+//     return cb(null, user);
+//   });
+// });
+
+// router.get("/login", passport.authenticate("openidconnect"));
+
+// router.get(
+//   "/oauth2/redirect",
+//   passport.authenticate("openidconnect", {
+//     successRedirect: "/",
+//     failureRedirect: "/login",
+//   })
+// );
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const path = require("path");
 const express = require("express");
 const routes = require('./controllers/')
 const exphbs = require("express-handlebars");
+var logger = require("morgan");
+var session = require("express-session");
+// var passport = require("passport");
+
+// var SQLiteStore = require("connect-sqlite3")(session);
+
+var indexRouter = require("./controllers/index");
+var authRouter = require("./controllers/home-routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,9 +21,20 @@ const hbs = exphbs.create({});
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+// app.use(session({
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: false,
+//   store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+// }));
+// app.use(passport.authenticate("session"));
+
+app.use("/", indexRouter);
+app.use("/", authRouter);
 
 app.use(routes);
 
