8cb9587213a7cb6eda238369aeeea57ed342f1f4
Elexis Blash <elexisblash@gmail.com>
2022-05-17 19:24:34 -0400

    Fixed signup.js and added links to hero buttons

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,37 +1,6 @@
-<<<<<<< HEAD
-async function signupFormHandler(event) {
+function signupFormHandler(event) {
   event.preventDefault();
 
-  const username = document.querySelector("#username-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
-  const phone = document.querySelector("#phone-signup").value.trim();
-
-  if (username && email && password) {
-    await fetch("/api/owners", {
-      method: "post",
-      body: JSON.stringify({
-        username,
-        email,
-        password,
-        phone,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-    if (response.ok) {
-      console.log("success");
-=======
-async function loginFormHandler(event) {
-  event.preventDefault();
-
-<<<<<<<< HEAD:public/js/login.js
-  const username = document.querySelector("#username-login").value.trim();
-  const password = document.querySelector("#password-login").value.trim();
-
-  if (username && password) {
-    const response = await fetch("/api/owners/login", {
-      method: "post",
-========
   const username = document.querySelector("#username-signup").value.trim();
   const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
@@ -42,32 +11,17 @@ async function loginFormHandler(event) {
   if (username && email && password && first_name && last_name) {
     fetch('/api/owners', {
       method: 'post',
->>>>>>>> 4bf890b35b4c8ceafc36fc2616230f21b380bcda:public/js/signup.js
       body: JSON.stringify({
         username,
+        email,
         password,
-<<<<<<<< HEAD:public/js/login.js
-========
         first_name,
         last_name,
         phone
->>>>>>>> 4bf890b35b4c8ceafc36fc2616230f21b380bcda:public/js/signup.js
       }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok) {
-      console.log("success");
-      document.location.replace("/");
->>>>>>> 4bf890b35b4c8ceafc36fc2616230f21b380bcda
-    } else {
-      alert(response.statusText);
-    }
+      headers: { 'Content-Type': 'application/json' }
+    }).then((response) => {console.log(response)})
   }
 }
 
-<<<<<<< HEAD
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-=======
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
->>>>>>> 4bf890b35b4c8ceafc36fc2616230f21b380bcda
